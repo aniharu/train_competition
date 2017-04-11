6df82bb2b18bbeae393e88a64b81d5bf3de13e82
aniharu <haruya04183083@gmail.com>
2017-04-11 14:23:38 +0900

    auto format修正

--- data_analysis.py
+++ data_analysis.py
@@ -1,5 +1,5 @@
-#coding:utf-8
-#各データの形を見る
+# coding:utf-8
+# 各データの形を見る
 
 import seaborn as sns
 import numpy as np
@@ -12,94 +12,98 @@ from myclass import myclass
 class deta_analysis(myclass):
     def __init__(self):
         super().__init__()
-    #詳細障害の棒グラフを表示する関数(acc:Trueで障害なし表示)
-    def show_circle_dt(self,acc):
-        labels=[]
-        nums=[]
+
+    # 詳細障害の棒グラフを表示する関数(acc:Trueで障害なし表示)
+    def show_circle_dt(self, acc):
+        labels = []
+        nums = []
         for i in self.detail.columns:
-            labels.append(self.get_ref(int(i),True))
+            labels.append(self.get_ref(int(i), True))
             nums.append(self.detail[i].ix[0])
-        #２つのリストをzipでまとめてソートする
-        tmp=list(zip(labels,nums))
-        tmp.sort(key=itemgetter(1),reverse=True)
-        labels,nums=zip(*tmp)
+        # ２つのリストをzipでまとめてソートする
+        tmp = list(zip(labels, nums))
+        tmp.sort(key=itemgetter(1), reverse=True)
+        labels, nums = zip(*tmp)
         sns.set(font='TakaoExMincho')
         if acc:
-            ax=sns.barplot(y=labels,x=nums)
+            ax = sns.barplot(y=labels, x=nums)
         else:
-            ax=sns.barplot(y=labels[1:], x=nums[1:])
+            ax = sns.barplot(y=labels[1:], x=nums[1:])
         total = 0
         for p in ax.patches:
             total += p.get_width()
         for p in ax.patches:
             width = p.get_width()
             ax.text(width + total / 50,
-                    p.get_y() + p.get_height() / 2+0.1,
+                    p.get_y() + p.get_height() / 2 + 0.1,
                     '{:1.2f}%'.format(width / total * 100),
                     ha="center")
         plt.title('各障害の総発生時間(詳細)')
         plt.xlabel('時間(10分)')
         plt.show()
-    #カテゴリ障害の棒グラフを表示する関数(acc:Trueで障害なし表示)
+
+    # カテゴリ障害の棒グラフを表示する関数(acc:Trueで障害なし表示)
     def show_circle_ct(self, acc):
-        dic={'異常なし':0,'人身支障':0,'機械支障':0,'気象支障':0}
+        dic = {'異常なし': 0, '人身支障': 0, '機械支障': 0, '気象支障': 0}
         for i in self.detail.columns:
-            dic[self.get_ref(int(i), False)]+=self.detail[i].ix[0]
+            dic[self.get_ref(int(i), False)] += self.detail[i].ix[0]
         # ２つのリストをzipでまとめてソートする
         tmp = list(zip(dic.keys(), dic.values()))
         tmp.sort(key=itemgetter(1), reverse=True)
         labels, nums = zip(*tmp)
         sns.set(font='TakaoExMincho')
         if acc:
-            ax=sns.barplot(y=labels, x=nums)
+            ax = sns.barplot(y=labels, x=nums)
         else:
-            ax=sns.barplot(y=labels[1:], x=nums[1:])
-        total=0
+            ax = sns.barplot(y=labels[1:], x=nums[1:])
+        total = 0
         for p in ax.patches:
-            total+=p.get_width()
+            total += p.get_width()
         for p in ax.patches:
-            width=p.get_width()
-            ax.text(width + total/30,
+            width = p.get_width()
+            ax.text(width + total / 30,
                     p.get_y() + p.get_height() / 2.,
-                    '{:1.3f}%'.format(width / total*100),
+                    '{:1.3f}%'.format(width / total * 100),
                     ha="center")
         plt.title('各障害の総発生時間')
         plt.xlabel('時間(10分)')
         plt.show()
-    #カテゴリ障害の平均時間を求める関数
+
+    # カテゴリ障害の平均時間を求める関数
     def calc_acctime_ct(self):
-        isacc=False
-        occur={'異常なし': 0, '人身支障': 0, '機械支障': 0, '気象支障': 0}
+        isacc = False
+        occur = {'異常なし': 0, '人身支障': 0, '機械支障': 0, '気象支障': 0}
         dic = {'異常なし': 0, '人身支障': 0, '機械支障': 0, '気象支障': 0}
         for i in self.detail.columns:
             dic[self.get_ref(int(i), False)] += self.detail[i].ix[0]
-        for i in range(1,6):
+        for i in range(1, 6):
             for j in range(len(self.dt)):
-                if isacc==False:
-                    if self.dt.ix[j,i] != 0:
-                        occur[self.get_ref(int(self.dt.ix[j,i]),False)] += 1
-                        isacc=True
+                if isacc == False:
+                    if self.dt.ix[j, i] != 0:
+                        occur[self.get_ref(int(self.dt.ix[j, i]), False)] += 1
+                        isacc = True
                 else:
-                    if self.dt.ix[j,i] == 0:
-                        isacc=False
-        a=np.array(list(dic.values()))
-        b=np.array(list(occur.values()))
-        result=1.0*a[1:]/b[1:]
+                    if self.dt.ix[j, i] == 0:
+                        isacc = False
+        a = np.array(list(dic.values()))
+        b = np.array(list(occur.values()))
+        result = 1.0 * a[1:] / b[1:]
         sns.set(font='TakaoExMincho')
-        sns.barplot(y=list(dic.keys())[1:],x=result)
+        sns.barplot(y=list(dic.keys())[1:], x=result)
         plt.title('各障害の平均発生時間')
         plt.xlabel('時間(10分)')
         plt.show()
+
     # 詳細障害の平均時間を求める関数
     def calc_acctime_dt(self):
         isacc = False
-        occur={}
+        occur = {}
         labels = []
         nums = []
         for i in self.detail.columns:
             labels.append(self.get_ref(int(i), True))
             nums.append(self.detail[i].ix[0])
-            occur[self.get_ref(int(i), True)]=0
+            occur[self.get_ref(int(i), True)] = 0
         for i in range(1, 6):
             for j in range(len(self.dt)):
                 if isacc == False:
@@ -109,7 +113,7 @@ class deta_analysis(myclass):
                 else:
                     if self.dt.ix[j, i] == 0:
                         isacc = False
-        nums=1.0*np.array(nums)/list(occur.values())
+        nums = 1.0 * np.array(nums) / list(occur.values())
         # ２つのリストをzipでまとめてソートする
         tmp = list(zip(labels, nums))
         tmp.sort(key=itemgetter(1), reverse=True)
@@ -121,6 +125,6 @@ class deta_analysis(myclass):
         plt.show()
 
 
-if __name__=='__main__':
-    my=deta_analysis()
+if __name__ == '__main__':
+    my = deta_analysis()
     my.show_circle_dt(acc=False)
--- predict.py
+++ predict.py
@@ -1,32 +1,32 @@
-#coding:utf-8
-#よそくするお
+# coding:utf-8
+# よそくするお
 
 from random_forest import RandomForestC
 from SVM import SVM
 import pandas as pd
 import numpy as np
 
-if __name__=='__main__':
-    my=RandomForestC()
-    #my=SVM()
+if __name__ == '__main__':
+    my = RandomForestC()
+    # my=SVM()
     my.set_minimum(0.001)
     my.set_trees(800)
     my.set_features('auto')
     my.model_create()
-    my.fit(my.data.ix[:,6:],my.data[['tyuou','keihintohoku','keiyou','uchibou','saikyoukawagoe']])
-    result=my.predict(my.data.ix[:,6:])
-    data=pd.read_csv('data/sample_submit.csv',names=['name','1','2','3','4'],dtype={'name':str})
-    submit=pd.DataFrame()
-    submit['name']=data['name'].tolist()
-    result=result.tolist()
-    tmp=list(result)
+    my.fit(my.data.ix[:, 6:], my.data[['tyuou', 'keihintohoku', 'keiyou', 'uchibou', 'saikyoukawagoe']])
+    result = my.predict(my.data.ix[:, 6:])
+    data = pd.read_csv('data/sample_submit.csv', names=['name', '1', '2', '3', '4'], dtype={'name': str})
+    submit = pd.DataFrame()
+    submit['name'] = data['name'].tolist()
+    result = result.tolist()
+    tmp = list(result)
     result.extend(tmp)
     result.extend(tmp)
     result.extend(tmp)
     result.extend(tmp)
-    result=np.array(result)
-    submit['1']=result[:,0]
-    submit['2'] = result[:,1]
-    submit['3'] = result[:,2]
-    submit['4'] = result[:,3]
-    submit.to_csv('submit.csv',index=False,header=False,float_format='%.10f')
+    result = np.array(result)
+    submit['1'] = result[:, 0]
+    submit['2'] = result[:, 1]
+    submit['3'] = result[:, 2]
+    submit['4'] = result[:, 3]
+    submit.to_csv('submit.csv', index=False, header=False, float_format='%.10f')
