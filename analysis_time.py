4520bf38a9a71e5f73971c2d2cda49725af73339
umeco <haruya04183083@gmail.com>
2017-04-16 23:31:37 +0900

    可視化完了

--- analysis_time.py
+++ analysis_time.py
@@ -2,15 +2,60 @@
 #データ時間相関分析
 
 import pandas as pd
-import datetime
 import tqdm
 import pickle
+import seaborn as sns
+import datetime
 
 class analysis_time():
     def __init__(self):
-        self.df = pd.read_csv('data/train.csv',parse_dates=True,index_col=0)
-    def read_data(self):
-        pass
+        self.read_data()
+    def read_data(self,create=False):
+        if create:
+            self.df = pd.read_csv('data/train.csv', parse_dates=True, index_col=0)
+        else:
+            with open('data/hour_data.pickle','rb') as f:
+                self.hour = pickle.load(f)
+            with open('data/days_data.pickle','rb') as f:
+                self.days = pickle.load(f)
+            with open('data/month_data.pickle','rb') as f:
+                self.month = pickle.load(f)
+            #データを可視化用に変換
+            tmp=[]
+            for i in range(24):
+                for j in range(1,4):
+                    if j==1:
+                        name='people'
+                    elif j==2:
+                        name='machine'
+                    else:
+                        name='weather'
+                    tmp.append([str(i+1),self.hour[i][j],name])
+            self.v_hour = pd.DataFrame(tmp,columns=['hour','acc_num','category'])
+
+            tmp = []
+            for i in range(7):
+                for j in range(1, 4):
+                    if j == 1:
+                        name = 'people'
+                    elif j == 2:
+                        name = 'machine'
+                    else:
+                        name = 'weather'
+                    tmp.append([self.fromweekdaytoname(i), self.days[i][j], name])
+            self.v_days = pd.DataFrame(tmp, columns=['days', 'acc_num', 'category'])
+
+            tmp = []
+            for i in range(12):
+                for j in range(1, 4):
+                    if j == 1:
+                        name = 'people'
+                    elif j == 2:
+                        name = 'machine'
+                    else:
+                        name = 'weather'
+                    tmp.append([i+1, self.month[i][j], name])
+            self.v_month = pd.DataFrame(tmp, columns=['month', 'acc_num', 'category'])
     def hour_data(self):
         #1h毎の発生頻度データ
         hour=[]
@@ -50,7 +95,39 @@ class analysis_time():
         print(month)
         with open('data/month_data.pickle', 'wb') as f:
             pickle.dump(month, f)
+    def view_data(self):
+        myorder=[]
+        for i in range(1,25):
+            myorder.append(str(i))
+        ax = sns.barplot(x='hour',y='acc_num',hue='category',order=myorder,data=self.v_hour)
+        sns.plt.title('hour vs num of accidents')
+        sns.plt.show()
+
+        ax = sns.barplot(x='days', y='acc_num', hue='category', data=self.v_days)
+        sns.plt.title('weekday vs num of accidents')
+        sns.plt.show()
+
+        ax = sns.barplot(x='month', y='acc_num', hue='category', data=self.v_month)
+        sns.plt.title('month vs num of accidents')
+        sns.plt.show()
+    def fromweekdaytoname(self,num):
+        if num==0:
+            return 'monday'
+        elif num==1:
+            return 'tuesday'
+        elif num==2:
+            return 'wendesday'
+        elif num==3:
+            return 'thursday'
+        elif num==4:
+            return 'friday'
+        elif num==5:
+            return 'saturday'
+        else:
+            return 'sunday'
+
+
 
 if __name__=='__main__':
     my=analysis_time()
-    my.month_data()
+    my.view_data()
